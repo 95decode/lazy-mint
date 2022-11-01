e0815fbea04c1edeb19cc0b17a1d045c077008e0
95decode <95decode@gmail.com>
2022-11-01 16:52:09 +0900

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,65 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
+import { BigNumber } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import "@nomicfoundation/hardhat-toolbox";
 
+task(
+  "accounts",
+  "Prints the list of accounts",
+  async (args, hre): Promise<void> => {
+      const accounts: SignerWithAddress[] = await hre.ethers.getSigners();
+      accounts.forEach((account: SignerWithAddress): void => {
+          console.log(account.address);
+      });
+  }
+);
+
+task(
+  "balances",
+  "Prints the list of account balances",
+  async (args, hre): Promise<void> => {
+      const accounts: SignerWithAddress[] = await hre.ethers.getSigners();
+      for (const account of accounts) {
+          const balance: BigNumber = await hre.ethers.provider.getBalance(
+              account.address
+          );
+          console.log(`${account.address} has balance ${balance.toString()}`);
+      }
+  }
+);
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [{
+      version: "0.8.0",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+          details: {
+            yul: true,
+            yulDetails : {
+              stackAllocation : true
+            },
+          },
+        },
+      },
+    },{
+      version: "0.8.17",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+          details: {
+            yul: true,
+            yulDetails : {
+              stackAllocation : true
+            },
+          },
+        },
+      },
+    }],
+  },
 };
 
 export default config;
